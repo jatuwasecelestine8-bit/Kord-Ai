a67397998ab18524c428497399a00e944bafb370
jatuwasecelestine8-bit <jatuwasecelestine8@gmail.com>
2025-10-02 04:47:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const { existsSync } = require('fs')
 if (existsSync('config.env')) require('dotenv').config({ path: './config.env' })
 
 module.exports = { 
-    SESSION_ID: process.env.SESSION_ID || '',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "2349067339193",
+    SESSION_ID: process.env.SESSION_ID || 'kord_ai-5H5MBVOodIO78ow4',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "2347032653843",
     WORKTYPE: process.env.WORKTYPE || "private",
     PREFIX: process.env.PREFIX || "[.]",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || true,
